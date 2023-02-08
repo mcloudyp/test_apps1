e344e3bf068d5d33cad0df661fdda6900047fcc4
yunpeng ma <mayunpeng@pjlab.org.cn>
2023-02-08 17:07:01 +0800

    modify app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import gradio as gr
 def greet(name):
-    # return "Hello" + name + "!!"
-    return False
+    return "Hello" + name + "!!"
     
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 iface.launch()
